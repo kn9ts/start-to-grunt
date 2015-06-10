e8f579afa142c7426967b5dc089bf02fc43ca78d
Wamburu <denzjoseph@gmail.com>
2015-06-10 16:59:33 -0700

    Initialized gulp file with defined func()

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+// Reference Gulp
+var gulp = require('gulp');
+
+// Reference plugins
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+
+// Compile CSS
+gulp.task('styles', function(){
+	...
+});
+
+// Optimize Images
+gulp.task('images', function(){
+	...
+});
