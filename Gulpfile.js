a01f51574c69c2f0f699fda56ab472069b8e1b56
Wamburu <denzjoseph@gmail.com>
2015-06-10 20:12:36 -0700

    JS Task added

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,7 +10,8 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
-
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 // Task Task
 /** The styles task looks for any .scss files in the app/styles directory of your project  
 * && preprocess them using sass and then saves the minified versions in the app/dist/styles dir
@@ -28,12 +29,21 @@ gulp.task('styles', function(){
     .pipe(gulp.dest('dist/styles/'))
 });
 
-
-
 // Image Task
 gulp.task('images', function(){
-  gulp.src('src/images/**/*')
+  gulp.src('app/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/images/'));
 });
-    
+
+// JS Tasking
+gulp.task('js', function(){
+  return gulp.src('app/js/**/*.js')
+    .pipe(concat('application.js'))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('dist/js/'))
+});
