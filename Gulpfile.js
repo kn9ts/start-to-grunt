8b9d06b94d8c9a0fd41e769ab222247f987e7062
Wamburu <denzjoseph@gmail.com>
2015-06-10 17:05:03 -0700

    Css Plugins reference added

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,21 @@
-// Reference Gulp
+// Gulp reference point
 var gulp = require('gulp');
 
-// Reference plugins
+// Plugins are referenced here
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
 
 // Compile CSS
 gulp.task('styles', function(){
 	...
 });
 
-// Optimize Images
+// Images optimization
 gulp.task('images', function(){
 	...
 });
